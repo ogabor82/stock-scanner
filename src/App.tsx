a6a33a9eb3593598d33f15bd23601be958692f56
Ocskó Gábor <ogabor@gmail.com>
2024-09-15 01:44:08 +0200

    react router/favorites

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import "./App.css";
+import { Favorites } from "./components/Favorites/Favorites";
 import { News } from "./components/News/News";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "",
+    element: <News />,
+  },
+  {
+    path: "/",
+    element: <News />,
+  },
+  {
+    path: "/favorites",
+    element: <Favorites />,
+  },
+]);
 
 function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <News />
+        <RouterProvider router={router} />
       </QueryClientProvider>
     </>
   );
---
+++ src/components/Favorites/Favorites.tsx
@@ -0,0 +1,8 @@
+export function Favorites() {
+  return (
+    <div>
+      <h1>Favorites</h1>
+      <p>This is a favorites page.</p>
+    </div>
+  );
+}
