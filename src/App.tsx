f39c47151f4c01165ecc18b017fb7dadead3bca0
Ocskó Gábor <ogabor@gmail.com>
2024-09-18 08:44:18 +0200

    code cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import { News } from "./components/News/News";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { SymbolPage } from "./components/Symbol/SymbolPage";
-import { Home } from "./components/Home/Home";
-import LeftNavigation from "./components/Menu/LeftNavigation";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { fetchFavorites } from "./store/favorite";
