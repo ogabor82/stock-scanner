9c0aa8b188a7cf663530dbf971f31687ede22fb2
Ocskó Gábor <ogabor@gmail.com>
2024-10-09 09:33:50 +0200

    dividend chart

--- src/components/Symbol/SymbolDividend/SymbolDividend.tsx
+++ src/components/Symbol/SymbolDividend/SymbolDividend.tsx
@@ -2,6 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { getDividends } from "../../../data/query";
 import { Table } from "@mantine/core";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
 export function SymbolDividend() {
   const { symbol } = useParams();
@@ -29,6 +38,30 @@ export function SymbolDividend() {
     <div>
       <h1>Symbol Dividend</h1>
 
+      <LineChart
+        width={1200}
+        height={300}
+        data={dividendList.reverse()}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="exDividendDate" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="amount"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+      </LineChart>
+
       <Table.ScrollContainer minWidth={800}>
         <Table verticalSpacing="xs">
           <Table.Thead>
@@ -44,10 +77,10 @@ export function SymbolDividend() {
             {dividendList.length > 0 ? (
               dividendList.map((item: any) => (
                 <Table.Tr key={item.exDividendDate}>
-                  <Table.Td>{item.ex_dividend_date}</Table.Td>
-                  <Table.Td>{item.declaration_date}</Table.Td>
-                  <Table.Td>{item.record_date}</Table.Td>
-                  <Table.Td>{item.payment_date}</Table.Td>
+                  <Table.Td>{item.exDividendDate}</Table.Td>
+                  <Table.Td>{item.declarationDate}</Table.Td>
+                  <Table.Td>{item.recordDate}</Table.Td>
+                  <Table.Td>{item.paymentDate}</Table.Td>
                   <Table.Td>{item.amount}</Table.Td>
                 </Table.Tr>
               ))
